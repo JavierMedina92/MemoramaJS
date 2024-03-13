0e1c660afbd27ef4d9b27034409e3f0d9d2bee88
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-12 19:04:56 -0700

    se agrega funcionalidad

--- js/app.js
+++ js/app.js
@@ -31,12 +31,32 @@ function activate(e) {
     }
 }
 
+function randomValue() {
+    let rnd = Math.floor(Math.random() * totalCards * 0.5);
+    let values = valuesUsed.filter(value => value === rnd);
+    if (values.length < 2) {
+        valuesUsed.push(rnd);
+    }
+    else {
+        randomValue();
+    }
+}
 
+function getFaceValue(value) {
+    let rtn = value;
+    if (value < availableCards.length) {
+       rtn = availableCards[value];
+    }
+    return rtn;
+ }
 
 for (let i=0; i < totalCards; i++) {
     let div = document.createElement('div');
     div.innerHTML = cardTemplate;
     cards.push(div);
     document.querySelector('#game').append(cards[i]);
+    randomValue();
+    cards[i].querySelectorAll('.face')[0].innerHTML = valuesUsed[i];
+    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
 }
 
