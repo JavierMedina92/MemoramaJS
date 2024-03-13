f9a7d2e242c2d46b44d4ff100514a0cec3ea458c
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-12 19:29:24 -0700

    se agregan funcionalidades para eficientar el juego

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let selectedCards = [];
 let valuesUsed = [];
 let currentMove = 0;
 
+
 let cardTemplate = '<div class="card"> <div class="back"></div><div class="face"></div></div>'
 
 function activate(e) {
@@ -13,18 +14,20 @@ function activate(e) {
         if(!selectedCards[0] || selectedCards[0] !== e.target) {
             selectedCards.push(e.target);
 
-            if(++currentMove ==2) {
-                if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
-                    selectedCards = [];
-                    currentMove = [];
-                }
-                else {
+            if (++currentMove === 2) {
+                const firstCardValue = selectedCards[0].querySelectorAll('.face')[0].innerHTML;
+                const secondCardValue = selectedCards[1].querySelectorAll('.face')[0].innerHTML;
+                
+                if (firstCardValue !== secondCardValue) {
                     setTimeout(() => {
                         selectedCards[0].classList.remove('active');
-                        selectedCards[0].classList.remove('active');
+                        selectedCards[1].classList.remove('active');
                         selectedCards = [];
                         currentMove = 0;
-                    }, 600);
+                    }, 2000); // Cambio aquí el tiempo de espera a 5 segundos (5000 ms)
+                } else {
+                    selectedCards = [];
+                    currentMove = 0;
                 }
             }
         }
@@ -60,3 +63,24 @@ for (let i=0; i < totalCards; i++) {
     cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
 }
 
+// document.getElementById('resetButton').addEventListener('click', resetGame);
+
+// function resetGame() {
+//     // Elimina la clase 'active' de todas las cartas
+//     document.querySelectorAll('.card').forEach(card => {
+//         card.classList.remove('active');
+//     });
+
+//     // Reinicia los arrays y las variables utilizadas para el juego
+//     selectedCards = [];
+//     valuesUsed = [];
+//     currentMove = 0;
+
+//     // Vuelve a generar las cartas
+//     generateCards();
+// }
+
+// // Función para generar las cartas
+// function generateCards() {
+//     // Código para generar las cartas...
+// }
